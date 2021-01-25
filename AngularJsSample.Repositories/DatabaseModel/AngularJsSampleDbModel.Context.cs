0770de353c72b346d48abd538d85167c21945cae
dgaz97 <45607502+dgaz97@users.noreply.github.com>
2021-01-25 20:51:43 +0100

    Model updated: Procedures added

--- AngularJsSample.Repositories/DatabaseModel/AngularJsSampleDbModel.Context.cs
+++ AngularJsSample.Repositories/DatabaseModel/AngularJsSampleDbModel.Context.cs
@@ -12,6 +12,8 @@ namespace AngularJsSample.Repositories.DatabaseModel
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Infrastructure;
+    using System.Data.Entity.Core.Objects;
+    using System.Linq;
     
     public partial class AngularJsSampleDbEntities : DbContext
     {
@@ -26,5 +28,56 @@ namespace AngularJsSample.Repositories.DatabaseModel
         }
     
         public virtual DbSet<UserInfo> UserInfoes { get; set; }
+    
+        public virtual ObjectResult<MovieAuthor_Get_Result> MovieAuthor_Get(Nullable<int> id)
+        {
+            var idParameter = id.HasValue ?
+                new ObjectParameter("Id", id) :
+                new ObjectParameter("Id", typeof(int));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<MovieAuthor_Get_Result>("MovieAuthor_Get", idParameter);
+        }
+    
+        public virtual ObjectResult<MovieAuthor_GetAll_Result> MovieAuthor_GetAll()
+        {
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<MovieAuthor_GetAll_Result>("MovieAuthor_GetAll");
+        }
+    
+        public virtual int MovieAuthor_Insert(Nullable<int> userId, string firstName, string lastName, Nullable<System.DateTime> birthDate, string birthPlace, string biography, string imdbUrl, string imageUrl)
+        {
+            var userIdParameter = userId.HasValue ?
+                new ObjectParameter("UserId", userId) :
+                new ObjectParameter("UserId", typeof(int));
+    
+            var firstNameParameter = firstName != null ?
+                new ObjectParameter("FirstName", firstName) :
+                new ObjectParameter("FirstName", typeof(string));
+    
+            var lastNameParameter = lastName != null ?
+                new ObjectParameter("LastName", lastName) :
+                new ObjectParameter("LastName", typeof(string));
+    
+            var birthDateParameter = birthDate.HasValue ?
+                new ObjectParameter("BirthDate", birthDate) :
+                new ObjectParameter("BirthDate", typeof(System.DateTime));
+    
+            var birthPlaceParameter = birthPlace != null ?
+                new ObjectParameter("BirthPlace", birthPlace) :
+                new ObjectParameter("BirthPlace", typeof(string));
+    
+            var biographyParameter = biography != null ?
+                new ObjectParameter("Biography", biography) :
+                new ObjectParameter("Biography", typeof(string));
+    
+            var imdbUrlParameter = imdbUrl != null ?
+                new ObjectParameter("ImdbUrl", imdbUrl) :
+                new ObjectParameter("ImdbUrl", typeof(string));
+    
+            var imageUrlParameter = imageUrl != null ?
+                new ObjectParameter("ImageUrl", imageUrl) :
+                new ObjectParameter("ImageUrl", typeof(string));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction("MovieAuthor_Insert", userIdParameter, firstNameParameter, lastNameParameter, birthDateParameter, birthPlaceParameter, biographyParameter, imdbUrlParameter, imageUrlParameter);
+        }
     }
 }
--- AngularJsSample.Repositories/DatabaseModel/AngularJsSampleDbModel.Designer.cs
+++ AngularJsSample.Repositories/DatabaseModel/AngularJsSampleDbModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\MatijaŠestak\source\repos\AngularJsSample\AngularJsSample.Repositories\DatabaseModel\AngularJsSampleDbModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Gazdek Denis\Desktop\praksa\Projekti\Angular-JS-Sample\AngularJsSample.Repositories\DatabaseModel\AngularJsSampleDbModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.

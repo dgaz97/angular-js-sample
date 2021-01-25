75bc64ae8acc6ecaa13f29a1e0b0073cd691ffda
dgaz97 <45607502+dgaz97@users.noreply.github.com>
2021-01-25 20:55:34 +0100

    Added repository for Movie Authors and implemented methods for adding movie authors, and getting single and all movie authors

---
+++ AngularJsSample.Model/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngularJsSample.Model
+{
+    public interface IRepository<T, TKey>
+    {
+        List<T> FindAll();
+        T FindBy(TKey key);
+        TKey Add(T item);
+        bool Delete(T item);
+        T Save(T item);
+    }
+}
---
+++ AngularJsSample.Repositories/MovieAuthors/MovieAuthorRepository.cs
@@ -0,0 +1,48 @@
+﻿using AngularJsSample.Model.MovieAuthors;
+using AngularJsSample.Repositories.DatabaseModel;
+using AngularJsSample.Repositories.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngularJsSample.Repositories.MovieAuthors
+{
+    public class MovieAuthorRepository : IMovieAuthorRepository
+    {
+        public int Add(MovieAuthor item)
+        {
+            using(var context = new AngularJsSampleDbEntities())
+            {
+                return context.MovieAuthor_Insert(item.UserCreated.Id, item.FirstName, item.LastName, item.BirthDate, item.BirthPlace, item.Biography, item.ImdbUrl, item.ImageUrl);
+            }
+        }
+
+        public bool Delete(MovieAuthor item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<MovieAuthor> FindAll()
+        {
+            using (var context = new AngularJsSampleDbEntities())
+            {
+                return context.MovieAuthor_GetAll().MapToModels();
+            }
+        }
+
+        public MovieAuthor FindBy(int key)
+        {
+            using (var context = new AngularJsSampleDbEntities())
+            {
+                return context.MovieAuthor_Get(key).SingleOrDefault().MapToModel();
+            }
+        }
+
+        public MovieAuthor Save(MovieAuthor item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

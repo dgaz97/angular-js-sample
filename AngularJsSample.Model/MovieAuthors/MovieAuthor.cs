28c6d939742dd030885d319136a3fb0eec50a846
dgaz97 <45607502+dgaz97@users.noreply.github.com>
2021-01-25 20:53:50 +0100

    Added Movie Author class in model

---
+++ AngularJsSample.Model/MovieAuthors/MovieAuthor.cs
@@ -0,0 +1,25 @@
+﻿using AngularJsSample.Model.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngularJsSample.Model.MovieAuthors
+{
+    public class MovieAuthor
+    {
+        public int Id { get; set; }
+        public UserInfo UserCreated { get; set; }
+        public DateTimeOffset DateCreated { get; set; }
+        public UserInfo UserLastModified { get; set; }
+        public DateTimeOffset? DateLastModified { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string BirthPlace { get; set; }
+        public string Biography { get; set; }
+        public string ImdbUrl { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}

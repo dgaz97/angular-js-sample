db8ff216f8509b6bc75b45548126a8e7bf45103a
dgaz97 <45607502+dgaz97@users.noreply.github.com>
2021-01-26 11:07:26 +0100

    Added interface for Movie Author service

---
+++ AngularJsSample.Services/IMovieAuthorService.cs
@@ -0,0 +1,17 @@
+﻿using AngularJsSample.Services.Messaging.MovieAuthors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngularJsSample.Services
+{
+    public interface IMovieAuthorService
+    {
+        GetAllMovieAuthorsResponse GetAllMovieAuthors(GetAllMovieAuthorsRequest request);
+        GetMovieAuthorResponse GetMovieAuthor(GetMovieAuthorRequest request);
+        SaveMovieAuthorResponse SaveMovieAuthor(SaveMovieAuthorRequest request);
+        DeleteMovieAuthorResponse DeleteMovieAuthor(DeleteMovieAuthorRequest request);
+    }
+}

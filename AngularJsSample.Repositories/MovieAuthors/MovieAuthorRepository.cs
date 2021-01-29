5238975a70dd603248cc2ff5b47bd4b30d4ea0ef
dgaz97 <45607502+dgaz97@users.noreply.github.com>
2021-01-29 11:39:21 +0100

    BackEnd Validation of data

--- AngularJsSample.Repositories/MovieAuthors/MovieAuthorRepository.cs
+++ AngularJsSample.Repositories/MovieAuthors/MovieAuthorRepository.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace AngularJsSample.Repositories.MovieAuthors
@@ -13,7 +14,9 @@ namespace AngularJsSample.Repositories.MovieAuthors
     {
         public int Add(MovieAuthor item)
         {
-            using(var context = new AngularJsSampleDbEntities())
+            if (item.Id != null || item.Id != 0) throw new Exception("Movie author ID must be null or 0");
+            checkDataForInsertOrUpdate(item);
+            using (var context = new AngularJsSampleDbEntities())
             {
                 return context.MovieAuthor_Insert(item.UserCreated.Id, item.FirstName, item.LastName, item.BirthDate, item.BirthPlace, item.Biography, item.ImdbUrl, item.ImageUrl, item.Popularity);
             }
@@ -23,8 +26,8 @@ namespace AngularJsSample.Repositories.MovieAuthors
         {
             using (var context = new AngularJsSampleDbEntities())
             {
-                int numberOfRowsEdited = context.MovieAuthor_Delete(item.Id, item.UserLastModified.Id);//Returns number of rows affected, should always be 1
-                if (numberOfRowsEdited == 0) throw new Exception("Movie author not found");
+                if (FindBy(item.Id) == null) throw new Exception($"Author {item.Id} doesn't exist");
+                context.MovieAuthor_Delete(item.Id, item.UserLastModified.Id);//Returns number of rows affected, should always be 1
                 return true;
             }
         }
@@ -49,9 +52,35 @@ namespace AngularJsSample.Repositories.MovieAuthors
         {
             using (var context = new AngularJsSampleDbEntities())
             {
+                if (item.Id == null || item.Id <= 0) throw new Exception("Movie author ID can't be null or negative");
+                if(FindBy(item.Id)==null) throw new Exception($"Author {item.Id} doesn't exist");
+                checkDataForInsertOrUpdate(item);
                 context.MovieAuthor_Update(item.Id, item.UserLastModified.Id, item.FirstName, item.LastName, item.BirthDate, item.BirthPlace, item.Biography, item.ImdbUrl, item.ImageUrl, item.Popularity);
                 return FindBy(item.Id);
             }
+
+        }
+        private void checkDataForInsertOrUpdate(MovieAuthor item)
+        {
+            if (item.FirstName == null || String.IsNullOrWhiteSpace(item.FirstName)) throw new Exception("First name can't be empty");
+            if (item.LastName == null || String.IsNullOrWhiteSpace(item.LastName)) throw new Exception("Last name can't be empty");
+            if (item.BirthDate == DateTime.MinValue || item.BirthDate == null) throw new Exception("Birth date can't be empty");
+            if (item.BirthPlace == null || String.IsNullOrWhiteSpace(item.BirthPlace)) throw new Exception("Birth place can't be empty");
+
+
+
+            if (item.ImdbUrl == null || String.IsNullOrWhiteSpace(item.ImdbUrl)) throw new Exception("IMDb url can't be empty");
+            Regex rxImdb = new Regex(@"^https?:\/\/(www\.)?imdb.com/");
+            if (!rxImdb.IsMatch(item.ImdbUrl)) throw new Exception("IMDb url is invalid");
+
+            if (!String.IsNullOrWhiteSpace(item.ImageUrl))
+            {
+                Regex rxHttp = new Regex(@"^https?:\/\/");
+                Regex rxImage = new Regex(@"\.jpg$|\.jpeg$|\.png$|\.gif$");
+                if (!rxHttp.IsMatch(item.ImageUrl) || !rxImage.IsMatch(item.ImageUrl)) throw new Exception("Image url is invalid");
+            }
+
+            if (item.Popularity == null || item.Popularity <= 0) throw new Exception("Popularity can't be empty, or less than 1");
         }
     }
 }

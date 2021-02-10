57127caae2da53e1380efaa0600a6f503cff99d8
dgaz97 <45607502+dgaz97@users.noreply.github.com>
2021-02-10 13:47:45 +0100

    Added hub class for refreshing author list, when author list changes

---
+++ AngularJsSample.Api/Hubs/MyHub.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AngularJsSample.Api.Hubs
+{
+    public class MyHub : Hub
+    {
+        /*public void Hello()
+        {
+            Clients.All.hello();
+        }*/
+
+        /*public void Send(string name, string message)
+        {
+            Clients.All.sendMessage(name, message);
+        }*/
+        public void Refresh()
+        {
+            Clients.All.updateMovieAuthors();
+        }
+        /*public void Subscribe(string customerId)
+        {
+            Groups.Add(Context.ConnectionId, customerId);
+        }*/
+
+        /*public void Unsubscribe(string customerId)
+        {
+            Groups.Remove(Context.ConnectionId, customerId);
+        }*/
+    }
+}

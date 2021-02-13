77f81c85eb51caf9830de058306c6a096dd96245
dgaz97 <45607502+dgaz97@users.noreply.github.com>
2021-02-13 10:26:44 +0100

    Setup IoC

--- AngularJsSample.Api/IoC/CommonModule.cs
+++ AngularJsSample.Api/IoC/CommonModule.cs
@@ -1,7 +1,13 @@
-﻿using AngularJsSample.Model.MoviePersons;
+﻿using AngularJsSample.Model.Genres;
+using AngularJsSample.Model.MoviePersons;
+using AngularJsSample.Model.MovieRatings;
+using AngularJsSample.Model.Movies;
 using AngularJsSample.Model.Users;
 using AngularJsSample.Repositories;
+using AngularJsSample.Repositories.Genres;
 using AngularJsSample.Repositories.MoviePersons;
+using AngularJsSample.Repositories.MovieRatings;
+using AngularJsSample.Repositories.Movies;
 using AngularJsSample.Services;
 using AngularJsSample.Services.Impl;
 using Autofac;
@@ -15,10 +21,16 @@ namespace AngularJsSample.Api.IoC
             //repositories
             builder.RegisterType<UserRepository>().As<IUserRepository>();
             builder.RegisterType<MoviePersonRepository>().As<IMoviePersonRepository>();
+            builder.RegisterType<GenreRepository>().As<IGenreRepository>();
+            builder.RegisterType<MovieRepository>().As<IMovieRepository>();
+            builder.RegisterType<MovieRatingRepository>().As<IMovieRatingRepository>();
 
             //services
             builder.RegisterType<UserService>().As<IUserService>();
             builder.RegisterType<MoviePersonService>().As<IMoviePersonService>();
+            builder.RegisterType<GenreService>().As<IGenreService>();
+            builder.RegisterType<MovieService>().As<IMovieService>();
+            builder.RegisterType<MovieRatingService>().As<IMovieRatingService>();
         }
     }
 }

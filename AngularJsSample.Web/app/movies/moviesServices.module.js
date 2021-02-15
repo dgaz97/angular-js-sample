23b77eab70f2a85bd05be1d5ac23c3f58c26387e
dgaz97 <45607502+dgaz97@users.noreply.github.com>
2021-02-15 16:36:31 +0100

    Added movie services to frontend

---
+++ AngularJsSample.Web/app/movies/moviesServices.module.js
@@ -0,0 +1,41 @@
+﻿(function () {
+
+    'use script';
+
+    angular
+        .module('moviesServices', [])
+        .service('moviesSvc', moviesSvc)
+        ;
+
+    moviesSvc.$inject = ['$http'];
+    function moviesSvc($http) {
+        this.getMovies = function () {
+            return $http.get(`${serviceBase}/api/movies`);
+        }
+        this.getMovie = function (id) {
+            return $http.get(`${serviceBase}/api/movies/${id}`);
+        }
+        this.getGenresOfMovie = function (id) {
+            return $http.get(`${serviceBase}/api/movies/genres/${id}`);
+        }
+        this.deleteMovie = function (id) {
+            return $http.delete(`${serviceBase}/api/movies/${id}`);
+        }
+        this.createMovie = function (movie) {
+            return $http.post(`${serviceBase}/api/movies`, movie);
+        }
+        this.updateMovie = function (id, movie) {
+            return $http.put(`${serviceBase}/api/movies/${id}`, movie);
+        }
+        this.addGenreToMovie = function (movieId, genreId) {
+            return $http.put(`${serviceBase}/api/movies/${movieId}/${genreId}`);
+        }
+        this.removeGenreFromMovie = function (movieId, genreId) {
+            return $http.delete(`${serviceBase}/api/movies/${movieId}/${genreId}`);
+        }
+    }
+
+
+
+
+})();

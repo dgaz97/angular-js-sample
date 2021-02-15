9b8bb3ecc34116b08bdd18cef01aeb842ada7258
dgaz97 <45607502+dgaz97@users.noreply.github.com>
2021-02-15 17:23:48 +0100

    Added movie overview grid

--- AngularJsSample.Web/app/movies/movies.module.js
+++ AngularJsSample.Web/app/movies/movies.module.js
@@ -12,6 +12,44 @@
     moviesOverviewCtrl.$inject = ['$scope', 'moviesSvc'];
     function moviesOverviewCtrl($scope, moviesSvc) {
         var vm = this;
+
+        moviesSvc.getMovies().then(function (result) {
+            console.log(result.data);
+            $scope.movieGridData = new kendo.data.DataSource({
+                data: result.data.movies,
+                pageSize: 5
+            })
+        })
+
+        $scope.movieGridOptions = {
+            dataBound: function () {
+                for (var i = 0; i < this.columns.length; i++) {
+                    if (i == 1) continue;
+                    this.autoFitColumn(i);
+                }
+            },
+            columns: [
+                {
+                    field: "movieName",
+                    title: "Naziv",
+                    template: '<a ui-sref="movieProfile({ id:#: data.movieId#})" href=" /movies/#: data.movieId#">#: data.movieName#</a>'
+                },
+                {
+                    field: "movieDescription",
+                    title: "Opis",
+                    //template: '<a ui-sref="movieProfile({ id:#: data.id#})" href=" /movies/#: data.id#">#: data.movieDescription#</a>'
+                },
+                {
+                    field: "movieReleaseDate",
+                    title: "Mjesto rođenja",
+                    template: '#= kendo.toString(kendo.parseDate(data.movieReleaseDate), "dd.MM.yyyy.") #'
+                },
+                {
+                    field: "movieRating",
+                    title: "Ocjena",
+                },
+            ]
+        }
     }
 
     movieProfileCtrl.$inject = ['$scope', '$state', 'movie', 'moviesSvc', '$stateParams'];

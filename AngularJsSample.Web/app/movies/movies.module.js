091546d26eefe69ff563259afa3335bd16651249
dgaz97 <45607502+dgaz97@users.noreply.github.com>
2021-02-15 16:36:58 +0100

    Added placeholder controllers and partials for movies

---
+++ AngularJsSample.Web/app/movies/movies.module.js
@@ -0,0 +1,33 @@
+﻿(function () {
+
+    'use strict';
+
+    angular
+        .module('movies', ['moviesServices', 'angularValidator'])
+        .controller('moviesOverviewCtrl', moviesOverviewCtrl)
+        .controller('movieProfileCtrl', movieProfileCtrl)
+        .controller('movieManageCtrl', movieManageCtrl)
+        ;
+
+    moviesOverviewCtrl.$inject = ['$scope', 'moviesSvc'];
+    function moviesOverviewCtrl($scope, moviesSvc) {
+        var vm = this;
+    }
+
+    movieProfileCtrl.$inject = ['$scope', '$state', 'movie', 'moviesSvc', '$stateParams'];
+    function movieProfileCtrl($scope, $state, movie, moviesSvc, $stateParams) {
+        var vm = this;
+        vm.movie = movie;
+    }
+
+    movieManageCtrl.$inject = ['$scope', '$state', 'movie', 'moviesSvc', '$stateParams'];
+    function movieManageCtrl($scope, $state, movie, moviesSvc, $stateParams) {
+        var vm = this;
+
+        vm.movie = movie ? movie : null;
+        vm.title = vm.movie ? true : false;
+
+
+    }
+
+}) ();

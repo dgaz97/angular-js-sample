3efd45f96433d128694d72569a06cd75b42be0f9
dgaz97 <45607502+dgaz97@users.noreply.github.com>
2021-02-25 20:15:49 +0100

    Added request and response for getting role of person in movie

---
+++ AngularJsSample.Services/Messaging/MovieRoles/Requests/GetRoleOfPersonInMovieRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngularJsSample.Services.Messaging.MovieRoles.Requests
+{
+    public class GetRoleOfPersonInMovieRequest:RequestBase
+    {
+        public int MoviePersonId { get; set; }
+        public int MovieId { get; set; }
+    }
+}
